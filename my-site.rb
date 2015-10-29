8d56ab83f583a4334e4167bd9d67f3a95e7e4a8b
Ricky Hougland <houglande@gmail.com>
2015-10-29 14:53:20 -0700

    finished adding layout.erb

--- my-site.rb
+++ my-site.rb
@@ -3,22 +3,27 @@ require "sinatra"
 class MySite < Sinatra::Base
 
   get "/" do
+    @title = "Ricky's Homepage"
     erb :index
   end
 
   get "/index" do
+    @title = "Ricky's Homepage"
     erb :index
   end
 
   get "/about" do
+    @title = "About Ricky"
     erb :about
   end
 
   get "/blog" do
+    @title = "Ricky's Blog"
     erb :blog
   end
 
   get "/projects" do
+    @title = "Ricky's Projects"
     erb :projects
   end
 

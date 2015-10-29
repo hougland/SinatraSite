c04ba6bb77e923869f05b5152c7c7d1ca29a56ef
Ricky Hougland <houglande@gmail.com>
2015-10-29 16:02:15 -0700

    add random page, starting to add guestbook

--- my-site.rb
+++ my-site.rb
@@ -2,27 +2,36 @@ require "sinatra"
 
 class MySite < Sinatra::Base
 
+  def all_sites
+    @all_sites = ["index", "about", "blog", "projects"]
+  end
+
   get "/" do
+    all_sites
     @title = "Ricky's Homepage"
     erb :index
   end
 
   get "/index" do
+    all_sites
     @title = "Ricky's Homepage"
     erb :index
   end
 
   get "/about" do
+    all_sites
     @title = "About Ricky"
     erb :about
   end
 
   get "/blog" do
+    all_sites
     @title = "Ricky's Blog"
     erb :blog
   end
 
   get "/projects" do
+    all_sites
     @title = "Ricky's Projects"
     erb :projects
   end
